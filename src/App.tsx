b376abb2f789b2d5305ebae0556807c66f5f344e
otacleT <mytspe46@gmail.com>
2023-11-17 13:55:36 +0900

    add: 基本形、スタイル追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import { useRef } from 'react';
-import './App.css';
+import { useCallback, useRef } from "react";
+import "./App.css";
 
 function App() {
   const dialogRef = useRef<HTMLDialogElement>(null);
 
-  const openModal = () => {
+  const openModal = useCallback(() => {
     if (dialogRef.current) {
       dialogRef.current.showModal();
     }
-  }
+  }, []);
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     if (dialogRef.current) {
       dialogRef.current.close();
     }
-  }
-  
+  }, []);
+
   return (
     <>
       <button onClick={() => openModal()}>Open Modal</button>
-      <dialog className='c-dialog' ref={dialogRef}>
-        <h2>タイトル</h2>
-        <p>このモーダルは、HTMLのdialog要素を使って実装されています。</p>
-        <button onClick={() => closeModal()}>Close Modal</button>
+      <dialog className="c-dialog" ref={dialogRef}>
+        <div className="c-dialog__inner">
+          <h2>タイトル</h2>
+          <p>このモーダルは、HTMLのdialog要素を使って実装されています。</p>
+          <button onClick={() => closeModal()}>Close Modal</button>
+          <button
+            onClick={() => {
+              alert("成功");
+              closeModal();
+            }}
+          >
+            アクション
+          </button>
+        </div>
       </dialog>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
